776963994bee800b5ad9acbbc291bec5f93d7b9a
luckmer <woszczo04@gmail.com>
2020-09-05 22:17:34 +0200

    other

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import React from 'react';
-import './style/App.css';
-import PresentPanel from './components/presentPanel';
-import ListView from './components/listView';
+import React from "react";
+import Expense from "./page/Expense";
+const App = () => <Expense />;
 
-export default class App extends React.Component {
-  render(){
-    return (
-      <div className = "container">
-        {/* title */}
-          <div className = "tagText">budget App</div>
-          {/* app information */}
-          <div className ="row_present_panel">
-              <PresentPanel/>
-          </div>
-          <div className=" View_expense_total_value_list"  id = "expense_list">
-            <ListView/>
-          </div>
-      </div>
-    );
-  }
-
-}
-
-
-
-
+export default App;
